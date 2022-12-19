b10e3d3eacfb90ce48f2a037ea08c4a40544fa9a
Furkan ATAMAN <furknataman@gmail.com>
2022-12-19 20:57:32 +0300

    event page bug fixed

--- lib/pages/events/event_page.dart
+++ lib/pages/events/event_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:qr/db/db_model/db_model_events.dart';
 import 'package:qr/global/date_time_converter.dart';
@@ -15,6 +16,8 @@ class Eventspage extends ConsumerStatefulWidget {
 }
 
 class _Eventspage extends ConsumerState<Eventspage> {
+  final List<String> entries = <String>['A', 'B', 'C'];
+  final List<int> colorCodes = <int>[600, 500, 100];
   ClassModelEvents? event;
   _Eventspage({@required this.event});
   String? timeData;
@@ -31,7 +34,7 @@ class _Eventspage extends ConsumerState<Eventspage> {
     final userInfo = ref.watch<UserInfo>(userInfoConfig);
     ClassTime time = classConverter(event!.dateTime!, event!.duration!);
     return Scaffold(
-      floatingActionButtonLocation: FloatingActionButtonLocation.miniStartTop,
+      /*floatingActionButtonLocation: FloatingActionButtonLocation.miniStartTop,
       floatingActionButton: IconButton(
         icon: const Icon(
           Icons.arrow_circle_left_outlined,
@@ -41,75 +44,83 @@ class _Eventspage extends ConsumerState<Eventspage> {
         onPressed: () {
           Navigator.pop(context);
         },
-      ),
-      body: Column(
-        children: [
-          Container(
-            width: MediaQuery.of(context).size.width,
-            height: 300,
-            decoration: BoxDecoration(
-                image: DecorationImage(
-                    colorFilter: ColorFilter.mode(
-                        Colors.white.withOpacity(0.75), BlendMode.dstATop),
-                    image: NetworkImage(
-                      event!.imageUrl!,
-                    ),
-                    fit: BoxFit.fitHeight),
-                color: Colors.black,
-                borderRadius: const BorderRadius.only(
-                    bottomLeft: Radius.circular(15), bottomRight: Radius.circular(15))),
-          ),
-          Expanded(
-            child: SingleChildScrollView(
-              child: Padding(
-                padding: const EdgeInsets.only(top: 23.0, left: 17, right: 17),
-                child: Column(
-                  children: [
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Text(
-                          event!.name.toString(),
-                          style: Theme.of(context).textTheme.displayLarge,
+      ),*/
+      body: CustomScrollView(
+        slivers: <Widget>[
+          SliverAppBar(
+            backgroundColor: const Color(0xff485FFF),
+            pinned: true,
+            expandedHeight: 160.0,
+            flexibleSpace: FlexibleSpaceBar(
+              //title: Text('SliverAppBar'),
+              background: Container(
+                width: MediaQuery.of(context).size.width,
+                decoration: BoxDecoration(
+                    image: DecorationImage(
+                        colorFilter: ColorFilter.mode(
+                            Colors.white.withOpacity(0.75), BlendMode.dstATop),
+                        image: NetworkImage(
+                          event!.imageUrl!,
                         ),
-                        RegisterButton(userInfo: userInfo, event: event),
-                      ],
-                    ),
-                    textContainer(
-                        "Date of Event", Theme.of(context).textTheme.displayMedium),
-                    textContainer(
-                        "${time.month.toString()} ${time.day.toString()}th ${time.clock.toString()}   ",
-                        Theme.of(context).textTheme.titleSmall,
-                        bottomPadding: 10),
-                    textContainer("Speakers", Theme.of(context).textTheme.displayMedium),
-                    textContainer(
-                        "Description", Theme.of(context).textTheme.displayMedium),
-                    textContainer(event!.description!.toString(),
-                        Theme.of(context).textTheme.titleSmall,
-                        bottomPadding: 10),
-                    Container(
-                      alignment: Alignment.centerLeft,
-                      padding: const EdgeInsets.only(top: 17, bottom: 17),
-                      child: Text(
-                        "Where is ${event!.eventsLocation}?",
-                        style: Theme.of(context).textTheme.displayMedium,
-                      ),
-                    ),
-                    LocationWidget(event: event),
-                    Expanded(
-                      child: ListView.builder(
-                        itemBuilder: (context, index) {
-                          context;
-                          return Text(event!.speakers![index].toString());
-                        },
-                        itemCount: event!.speakers!.length,
-                      ),
-                    ),
-                  ],
-                ),
+                        fit: BoxFit.fitHeight),
+                    color: Colors.black,
+                    borderRadius: const BorderRadius.only(
+                        bottomLeft: Radius.circular(15),
+                        bottomRight: Radius.circular(15))),
               ),
             ),
           ),
+          SliverList(
+              delegate: SliverChildListDelegate([
+            Padding(
+              padding: const EdgeInsets.only(top: 10.0, left: 17, right: 17),
+              child: Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        event!.name.toString(),
+                        style: Theme.of(context).textTheme.displayLarge,
+                      ),
+                      RegisterButton(userInfo: userInfo, event: event),
+                    ],
+                  ),
+                  textContainer(
+                      "Date of Event", Theme.of(context).textTheme.displayMedium),
+                  textContainer(
+                      "${time.month.toString()} ${time.day.toString()}th ${time.clock.toString()}   ",
+                      Theme.of(context).textTheme.titleSmall,
+                      bottomPadding: 10),
+                  textContainer("Speakers", Theme.of(context).textTheme.displayMedium),
+                  SizedBox(
+                    height: 30,
+                    child: ListView.builder(
+                      scrollDirection: Axis.horizontal,
+                      itemBuilder: (context, index) {
+                        context;
+                        return Padding(
+                          padding: const EdgeInsets.only(right: 12.0),
+                          child: textContainer(" ${event!.speakers![index].toString()}",
+                              Theme.of(context).textTheme.titleSmall,
+                              bottomPadding: 10),
+                        );
+                      },
+                      itemCount: event!.speakers!.length,
+                    ),
+                  ),
+                  textContainer("Description", Theme.of(context).textTheme.displayMedium),
+                  textContainer(event!.description!.toString(),
+                      Theme.of(context).textTheme.titleSmall,
+                      bottomPadding: 10),
+                  textContainer("Where is ${event!.eventsLocation}?",
+                      Theme.of(context).textTheme.displayMedium,
+                      bottomPadding: 10),
+                  LocationWidget(event: event),
+                ],
+              ),
+            ),
+          ])),
         ],
       ),
     );
