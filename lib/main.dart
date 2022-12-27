2780889fdb132ae2044225d6878c71aec1d56a2c
Furkan ATAMAN <furknataman@gmail.com>
2022-12-27 15:43:33 +0300

    dark mode

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,6 @@ import 'authentication/authservice.dart';
 import 'notifiation/local_notification/notification.dart';
 
 void main() async {
-  
   WidgetsFlutterBinding.ensureInitialized();
   FlutterLocalNotificationsPlugin()
       .resolvePlatformSpecificImplementation<AndroidFlutterLocalNotificationsPlugin>()
@@ -24,6 +23,8 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
+      darkTheme: _darkTheme,
+      themeMode: ThemeMode.dark,
       theme: ThemeData(
           primarySwatch: Colors.pink,
           fontFamily: "Raleway",
@@ -52,3 +53,56 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+
+ThemeData _darkTheme = ThemeData(
+    brightness: Brightness.dark,
+    primarySwatch: Colors.pink,
+    fontFamily: "Raleway",
+  
+    primaryColor: Color.fromARGB(0, 0, 0, 25),
+    textTheme: const TextTheme(
+        displayLarge:
+            TextStyle(fontSize: 24, color: Color(0xffBDBDBD), fontWeight: FontWeight.w400),
+        displayMedium:
+            TextStyle(fontSize: 16, color: Color(0xffBDBDBD), fontWeight: FontWeight.w400),
+        displaySmall:
+            TextStyle(fontSize: 16, color: Color(0xffBDBDBD), fontWeight: FontWeight.w400),
+        titleSmall:
+            TextStyle(fontSize: 14, color: Color(0xffBDBDBD), fontWeight: FontWeight.w400),
+        bodyLarge:
+            TextStyle(fontSize: 18, color: Color(0xffBDBDBD), fontWeight: FontWeight.w400),
+        labelLarge:
+            TextStyle(fontSize: 22, color: Color(0xffBDBDBD), fontWeight: FontWeight.w700),
+        labelSmall:
+            TextStyle(fontSize: 10, color: Color(0xffBDBDBD), fontWeight: FontWeight.w400),
+        titleMedium:
+            TextStyle(fontSize: 18, color: Colors.black, fontWeight: FontWeight.w700),
+        titleLarge:
+            TextStyle(fontSize: 33, color: Color(0xffBDBDBD), fontWeight: FontWeight.w400),
+        labelMedium: TextStyle(
+            fontSize: 14, color: Color(0xffBDBDBD), fontWeight: FontWeight.w400)));
+
+ThemeData _lightTheme = ThemeData(
+    primarySwatch: Colors.pink,
+    fontFamily: "Raleway",
+    textTheme: const TextTheme(
+        displayLarge:
+            TextStyle(fontSize: 24, color: Color(0xff333333), fontWeight: FontWeight.w400),
+        displayMedium:
+            TextStyle(fontSize: 16, color: Color(0xff333333), fontWeight: FontWeight.w400),
+        displaySmall:
+            TextStyle(fontSize: 16, color: Color(0xff828282), fontWeight: FontWeight.w400),
+        titleSmall:
+            TextStyle(fontSize: 14, color: Color(0xff828282), fontWeight: FontWeight.w400),
+        bodyLarge:
+            TextStyle(fontSize: 18, color: Color(0xff333333), fontWeight: FontWeight.w400),
+        labelLarge:
+            TextStyle(fontSize: 22, color: Color(0xff333333), fontWeight: FontWeight.w700),
+        labelSmall:
+            TextStyle(fontSize: 10, color: Color(0xff333333), fontWeight: FontWeight.w400),
+        titleMedium:
+            TextStyle(fontSize: 18, color: Colors.black, fontWeight: FontWeight.w700),
+        titleLarge:
+            TextStyle(fontSize: 33, color: Color(0xff333333), fontWeight: FontWeight.w400),
+        labelMedium: TextStyle(
+            fontSize: 14, color: Color(0xff4F4F4F), fontWeight: FontWeight.w400)));
