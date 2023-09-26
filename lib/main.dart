7a065c485059b2c02c1a4e2f864b327ab9b0244b
Furkan ATAMAN <furknataman@gmail.com>
2023-09-26 13:51:02 +0300

    added langues files

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,15 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:qr/l10n/l10n.dart';
 import 'package:qr/pages/start/start_page.dart';
 import 'package:qr/theme/light_theme.dart';
 import 'authentication/authservice.dart';
 import 'notification/local_notification/notification.dart';
 import 'theme/dark_theme.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 final GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
 
@@ -28,8 +31,15 @@ class MyApp extends ConsumerWidget {
     return MaterialApp(
       routes: {
         '/start': (context) => const StartPage(),
-        // diğer rotalar...
       },
+      supportedLocales: L10n9.all,
+      locale: const Locale('em'),
+      localizationsDelegates: const [
+        AppLocalizations.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate
+      ],
       navigatorKey: navigatorKey,
       title: 'Autumn Teachers Conference',
       debugShowCheckedModeBanner: false,
